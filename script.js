90296bc0c94cb15043fd6c53db4244418434b7fa
Anton Lovric <antonlovri81@gmail.com>
2021-08-31 21:24:44 +0200

    Added animations while scrolling through projects.

--- script.js
+++ script.js
@@ -1,67 +1,76 @@
 let projectIndex = 0;
 
-function setImage(image, url){
-    image.src = url;
+function setImage(image, url) {
+  image.src = url;
 }
 
-function setDesc(element, description){
-    element.innerHTML = description;
+function setDesc(element, description) {
+  element.innerHTML = description;
 }
 
-function nextProject () {
-    let projectImage, projectDesc;
-    projectImage = document.getElementById("projectImage");
-    projectDesc = document.getElementById("projectDesc");
-    switch(projectIndex){
-        case 0:
-            
-            setImage(projectImage, "productLanding.png");
-            setDesc(projectDesc, "Product Landing Page");
-            
-            break;
-        case 1:
-            setImage(projectImage, "survey.png");
-            setDesc(projectDesc, "Survey Form");
-            break;
-        case 2:
-            setImage(projectImage, "codewars.png")
-            setDesc(projectDesc, "CodeWars link");
-            break;
-        case 3:
-            setImage(projectImage,"javascriptDocumentation.png")
-            setDesc(projectDesc, "Javascript Documentation");
-            break;
-
-    }
-    projectIndex++;
-    if (projectIndex > 3) projectIndex = 0;
+function slideAnimation(direction) {
+  var projectImage;
+  projectImage = document.getElementById("projectImage");
+  
+  setTimeout(function(){
+      projectImage.style.transform = "translateY(0%)";
+  },700);
+  projectImage.style.transform = "translateY(-100%)";
+  
 }
 
-function previousProject(){
-    let projectImage, projectDesc;
-    projectImage = document.getElementById("projectImage");
-    projectDesc = document.getElementById("projectDesc");
+function nextProject() {
+  let projectImage, projectDesc;
+  
+  projectImage = document.getElementById("projectImage");
+  projectDesc = document.getElementById("projectDesc");
+  slideAnimation(0);
+  switch (projectIndex) {
+    case 0:
+      setImage(projectImage, "productLanding.png");
+      setDesc(projectDesc, "Product Landing Page");
 
-    switch (projectIndex) {
-        case 0:
-            
-            break;
-        case 1:
-            setImage(projectImage,"javascriptDocumentation.png")
-            setDesc(projectDesc, "Javascript Documentation");
-            break;
-        case 2:
-            setImage(projectImage, "productLanding.png");
-            setDesc(projectDesc, "Product Landing Page");
-            
-            break;
-        case 3:
-            setImage(projectImage, "codewars.png")
-            setDesc(projectDesc, "CodeWars link");
-            break;
-    
-    }
+      break;
+    case 1:
+      setImage(projectImage, "survey.png");
+      setDesc(projectDesc, "Survey Form");
+      break;
+    case 2:
+      setImage(projectImage, "codewars.png");
+      setDesc(projectDesc, "CodeWars link");
+      break;
+    case 3:
+      setImage(projectImage, "javascriptDocumentation.png");
+      setDesc(projectDesc, "Javascript Documentation");
+      break;
+  }
+  projectIndex++;
+  if (projectIndex > 3) projectIndex = 0;
+}
 
-    projectIndex--;
-    if (projectIndex < 0) projectIndex = 0;
+function previousProject() {
+  let projectImage, projectDesc;
+  projectImage = document.getElementById("projectImage");
+  projectDesc = document.getElementById("projectDesc");
+
+  switch (projectIndex) {
+    case 0:
+      break;
+    case 1:
+      setImage(projectImage, "javascriptDocumentation.png");
+      setDesc(projectDesc, "Javascript Documentation");
+      break;
+    case 2:
+      setImage(projectImage, "productLanding.png");
+      setDesc(projectDesc, "Product Landing Page");
+
+      break;
+    case 3:
+      setImage(projectImage, "codewars.png");
+      setDesc(projectDesc, "CodeWars link");
+      break;
+  }
+
+  projectIndex--;
+  if (projectIndex < 0) projectIndex = 0;
 }
