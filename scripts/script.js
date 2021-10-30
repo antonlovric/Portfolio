0db355c94d42613bd35ddd221b423dd2b5ba3227
Anton Lovrić <antonlovri81@gmail.com>
2021-10-30 17:06:01 +0200

    Implemented the intersection observer API so now the header changes color depending on which section the user is looking at. Also added some extra styling to the projects section.

--- scripts/script.js
+++ scripts/script.js
@@ -40,3 +40,56 @@ function toggleOverlay(overlay) {
     refreshButtons(buttons, true);
   } else refreshButtons(buttons, false);
 }
+
+swiper.on("slideChange", updateProjectTitle);
+
+function updateProjectTitle() {
+  const index = swiper.realIndex;
+  const projectTitle = document.querySelector(".projectTitle");
+  switch (index) {
+    case 0:
+      projectTitle.innerText = "Alojzije Janković Project";
+      break;
+
+    case 1:
+      projectTitle.innerText = "BMI Calculator";
+      break;
+    case 2:
+      projectTitle.innerText = "Portfolio Website";
+      break;
+    case 3:
+      projectTitle.innerText = "Sunnyside Agency Landing Page";
+      break;
+    case 4:
+      projectTitle.innerText = "freeCodeCamp Certification";
+      break;
+    case 5:
+      projectTitle.innerText = "MOOC Certification";
+      break;
+  }
+}
+
+const target = document.querySelector("#bio");
+
+function handleIntersection(entries) {
+  console.log(entries);
+  const header = document.querySelector(".navbar");
+  console.log(header);
+  entries.forEach((entry) => {
+    if (entry.isIntersecting == true) {
+      header.classList.add("grayBackground");
+    } else {
+      header.classList.remove("grayBackground");
+    }
+  });
+}
+
+const options = {
+  root: null,
+  rootMargin: "0px 0px -80% 0px",
+  threshold: 0,
+};
+
+const observer = new IntersectionObserver(handleIntersection, options);
+
+observer.observe(target);
